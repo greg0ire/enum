1e36c893e49bb4f947d6c360f2a0cfba18c3811e
Grégoire Paris <postmaster@greg0ire.fr>
2017-11-03 20:23:21 +0100

    Drop support for php < 7.1

--- UPGRADE-4.0.md
+++ UPGRADE-4.0.md
@@ -4,3 +4,13 @@
 
 Support for Symfony < 3.2 has been dropped.
 Support for Twig < 2.0 has been dropped.
+Support for PHP < 7.1 has been dropped.
+
+## API
+
+Type hinting has been added to most classes.
+
+### `Greg0ire\Enum\AbstractEnum`
+
+`getEnumTypes` may no longer return a string, it should always return an array
+now.
--- src/Exception/InvalidEnumName.php
+++ src/Exception/InvalidEnumName.php
@@ -4,7 +4,7 @@ namespace Greg0ire\Enum\Exception;
 
 final class InvalidEnumName extends \UnexpectedValueException
 {
-    public static function fromName($name, array $supportedNames)
+    public static function fromName(string $name, array $supportedNames): self
     {
         throw new self(sprintf(
             '"%s" is not a valid name, valid names are: ("%s")',
